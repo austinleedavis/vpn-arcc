de383ed363884ab9d5d465240420d395d2a3fca2
Austin <austindavis@gmail.com>
2023-12-06 18:03:20 -0500

    Updated instructions

--- vpn.sh
+++ vpn.sh
@@ -4,31 +4,18 @@
 
 set -e
 
-
 salloc_command='salloc --job-name=Jupyter_Server --nodes=1 --ntasks-per-node=1 --mem=64GB --time=02:00:00'
 
 PIDFILE=/var/run/openconnect.pid
 
-test $UID -ne 0 && echo Must be root 👑 && exit 2
-
-test -f $PIDFILE && pgrep -aF $PIDFILE && echo openconnect already running 👹 && echo $salloc_command | xclip -selection clipboard && echo "Interactive session command copied to your clipboard 📋:
-    $salloc_command" && exit 3
-
-# javascript:window.alert(document.cookie.match(/webvpn=(.*?);/)[1])
-printf "Cookie 🍪: "
-read COOKIE
-
-test -z "$COOKIE" && echo Must offer cookie 👹🍪 && exit 4
-
-openconnect --background --reconnect-timeout=60 --non-inter --pfs --syslog --force-dpd=47 --pid-file=$PIDFILE --disable-ipv6 secure.vpn.ucf.edu --usergroup="UCF Students" -C $COOKIE
-
-echo $salloc_command | xclip -selection clipboard && echo "Interactive session command copied to your clipboard 📋:
+display_instructions() {
+    echo "=====================INSTRUCTIONS==============================="
+    echo $salloc_command | xclip -selection clipboard && echo "📋 Interactive session command copied to your clipboard :
     $salloc_command"
 
-{
     cat <<EOF
 
-You are now connected to the UCF VPN 🎉
+🎉 You are connected to the UCF VPN 
 
 Next step:
 -----------------------------------------------------------------------
@@ -39,11 +26,29 @@ Next step:
         --nodes=1
         --ntasks-per-node=1
         --gpus=1
-        --mem=128GB
-        --time = 08:30:00
-        --constraint=h100
+        --mem=64GB
+        --time = 02:00:00
  3. DO NOT close this terminal; salloc starts an INTERACTIVE session!
 -----------------------------------------------------------------------
-
+Shutdown:
+ 1. Clean up port forwarding with:
+    ssh newton.ist.ucf.edu -O exit
+ 2. Terminate VPN connection with:
+    sudo kill $(cat $PIDFILE)
+-----------------------------------------------------------------------
 EOF
-  }
+}
+
+test $UID -ne 0 && echo 👑 Must be root  && exit 2
+
+test -f $PIDFILE && pgrep -aF $PIDFILE > /dev/null && echo 👍 openconnect already running  && display_instructions && exit 3
+
+# javascript:window.alert(document.cookie.match(/webvpn=(.*?);/)[1])
+printf "Cookie 🍪: "
+read COOKIE
+
+test -z "$COOKIE" && echo Must offer cookie 👹🍪 && exit 4
+
+openconnect --background --reconnect-timeout=60 --non-inter --pfs --syslog --force-dpd=47 --pid-file=$PIDFILE --disable-ipv6 secure.vpn.ucf.edu --usergroup="UCF Students" -C $COOKIE
+
+display_instructions
